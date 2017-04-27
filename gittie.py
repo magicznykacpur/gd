508022c840ade9861f28f4fd39808fe8198a8a4a
Kacper Zelichowski <kafejek@gmail.com>
2017-04-27 09:22:46 +0200

    tera dobre

--- gittie.py
+++ gittie.py
@@ -2,17 +2,17 @@ def add():
     '''To bedzie dodawac he he xd'''
     addycja_kolalicja_od_tylu_pozycja(6, 9)
 
+
 def introduction():
     print("JP2 GMD")
 
-<<<<<<< HEAD
-introduction()
-=======
 
 def joke():
     print("Żartuję, nie były takie małe")
 
+
 introduction()
 
 joke()
->>>>>>> d66b8a4d1e640b581ab7575e4c3ecb558552c33c
+
+add()
